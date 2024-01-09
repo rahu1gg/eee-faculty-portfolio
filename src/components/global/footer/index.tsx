60eb6f646aaffac8ef9ff9e0c92d72babfd6c608
rahu1gg <rahulpalamarthi@gmail.com>
2024-01-09 22:12:54 +0530

    enhance: enhance footer section with faculty name and SVUCE EEE web page

--- src/components/global/footer/index.tsx
+++ src/components/global/footer/index.tsx
@@ -2,20 +2,22 @@ import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 
 export default function Footer() {
-	return (
-		<footer className='border-t border-dashed'>
-			<div className='mx-[4%] py-3'>
-				<div>
-					<p className='text-muted-foreground text-sm'>
-						Crafted by{' '}
-						<Button className='p-0 text-muted-foreground font-medium hover:text-foreground' variant='link' asChild>
-							<Link href={'/'} target='_black' rel='noreferrer'>
-								rahu1gg
-							</Link>
-						</Button>
-					</p>
-				</div>
-			</div>
-		</footer>
-	);
+    return (
+        <footer className='border-t border-dashed'>
+            <div className='max-w-maxi mx-auto px-[4%] py-6'>
+                <div className='flex items-center justify-between'>
+                    <p className='text-muted-foreground text-sm'>
+                        <span className='font-medium'>Dr. J.N. Chandrasekhar</span> (Assistant Professor)
+                    </p>
+                    <p className='text-muted-foreground text-sm'>
+                        <Button className='p-0 text-muted-foreground font-medium hover:text-foreground relative z-50' variant='link' asChild>
+                            <Link href={'https://www.svuce-eee.in'} target='_blank' rel='noreferrer'>
+                                SVUCE EEE
+                            </Link>
+                        </Button>
+                    </p>
+                </div>
+            </div>
+        </footer>
+    );
 }
