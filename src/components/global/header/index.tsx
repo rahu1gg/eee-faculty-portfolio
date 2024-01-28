abbbc09c1ddb6a5423dbc1f3c772f41d8c343841
rahu1gg <rahulpalamarthi@gmail.com>
2024-01-28 09:01:00 +0530

    styles: add three-d-card effect to header component and profile pic

--- src/components/global/header/index.tsx
+++ src/components/global/header/index.tsx
@@ -1,4 +1,5 @@
 import { NavLink } from '@/components/global/header/client';
+import { CardBody, CardContainer } from '@/components/ui/three-d-card';
 import { PORTFOLIO } from '@/constants/portfolio';
 import { allPortfolios } from 'contentlayer/generated';
 import Image from 'next/image';
@@ -7,41 +8,46 @@ import Link from 'next/link';
 export default function Header() {
     return (
         <header className='sticky top-5 m-5 h-hvh rounded-lg'>
-            <div className='relative h-full flex flex-col items-start p-5 justify-between bg-[#e5fa6c] rounded-lg overflow-hidden'>
-                <div>
-                    <Link href='/' className='text-sm font-bold relative -top-1'>
-                        {PORTFOLIO.name}
-                    </Link>
-                </div>
-                <nav className='w-full mt-auto'>
-                    <ul>
-                        {allPortfolios
-                            .sort((a, b) => a.index - b.index)
-                            .map((section) => (
-                                <li key={section._id}>
-                                    <Link href={section.href ?? section.url} className='group text-sm w-full inline-block py-0.5'>
-                                        <NavLink href={section.href ?? section.url}>{section.label}</NavLink>
-                                    </Link>
-                                </li>
-                            ))}
-                    </ul>
-                </nav>
-                <div className='pt-3 flex items-center justify-start gap-5 w-full'>
-                    {PORTFOLIO.socialLinks.map(({ icon, label, href }, index) => (
-                        <Link key={`${index}`} href={href} className='grayscale scale-100 hover:grayscale-0 hover:scale-105 duration-300'>
-                            <Image
-                                src={`/portfolio/social-links/${icon}`}
-                                className='size-6'
-                                width={20}
-                                height={20}
-                                alt={label}
-                                loading='lazy'
-                                unoptimized
-                            />
+            <CardContainer
+                className='h-full w-full'
+                containerClassName='py-0 relative h-full flex flex-col items-start p-7 justify-between bg-[#e5fa6c] rounded-lg overflow-hidden'
+            >
+                <CardBody className='h-full w-full flex flex-col items-start justify-between'>
+                    <div>
+                        <Link href='/' className='text-sm font-bold relative -top-1'>
+                            {PORTFOLIO.name}
                         </Link>
-                    ))}
-                </div>
-            </div>
+                    </div>
+                    <nav className='w-full mt-auto'>
+                        <ul>
+                            {allPortfolios
+                                .sort((a, b) => a.index - b.index)
+                                .map((section) => (
+                                    <li key={section._id}>
+                                        <Link href={section.href ?? section.url} className='group text-sm w-full inline-block py-0.5'>
+                                            <NavLink href={section.href ?? section.url}>{section.label}</NavLink>
+                                        </Link>
+                                    </li>
+                                ))}
+                        </ul>
+                    </nav>
+                    <div className='pt-3 flex items-center justify-start gap-5 w-full'>
+                        {PORTFOLIO.socialLinks.map(({ icon, label, href }, index) => (
+                            <Link key={`${index}`} href={href} className='grayscale scale-100 hover:grayscale-0 hover:scale-105 duration-300'>
+                                <Image
+                                    src={`/portfolio/social-links/${icon}`}
+                                    className='size-6'
+                                    width={20}
+                                    height={20}
+                                    alt={label}
+                                    loading='lazy'
+                                    unoptimized
+                                />
+                            </Link>
+                        ))}
+                    </div>
+                </CardBody>
+            </CardContainer>
         </header>
     );
 }
--- src/components/pages/portfolio-section/index.tsx
+++ src/components/pages/portfolio-section/index.tsx
@@ -1,3 +1,4 @@
+import { CardBody, CardContainer, CardItem } from '@/components/ui/three-d-card';
 import { HOME_SECTION_INDEX } from '@/constants/app';
 import { PORTFOLIO } from '@/constants/portfolio';
 import { allPortfolios } from 'contentlayer/generated';
@@ -16,15 +17,21 @@ export function HeroSection() {
         <div className='min-h-hvh flex items-stretch justify-center'>
             <div className='w-full pr-5 border-r border-[#e5fa6c] flex justify-between flex-col'>
                 <div className='text-end'>
-                    <Image
-                        src={`/portfolio/images/${PORTFOLIO.profilePic.src}`}
-                        className='inline-block w-48 h-auto rounded-lg'
-                        width={100}
-                        height={100}
-                        alt={PORTFOLIO.profilePic.alt}
-                        loading='lazy'
-                        unoptimized
-                    />
+                    <CardContainer containerClassName='py-0 justify-end'>
+                        <CardBody className='w-max h-auto'>
+                            <CardItem translateZ='50'>
+                                <Image
+                                    src={`/portfolio/images/${PORTFOLIO.profilePic.src}`}
+                                    className='inline-block w-48 h-auto rounded-lg'
+                                    width={100}
+                                    height={100}
+                                    alt={PORTFOLIO.profilePic.alt}
+                                    loading='lazy'
+                                    unoptimized
+                                />
+                            </CardItem>
+                        </CardBody>
+                    </CardContainer>
                 </div>
                 <h2 className='uppercase text-8xl font-bold bg-gradient-to-tr from-foreground to-muted-foreground w-max bg-clip-text text-transparent'>
                     Portfolio
